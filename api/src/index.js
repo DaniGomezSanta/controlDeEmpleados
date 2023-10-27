7e8fd7aac764b99ee8b5dda002c609762ddf4540
DanielaGomez <ledangom@gmail.com>
2023-10-27 14:37:41 -0500

    Se hace una modificacion en el puerto

--- api/src/index.js
+++ api/src/index.js
@@ -4,11 +4,13 @@ import { sequelize } from './database/database.js';
 // import './models/Empleado.js'
 // import './models/Autorizacion.js'
 
+const port = process.env.PORT || 3000; 
+
 async function main() {
     try {
         await sequelize.sync({force: true});
-        app.listen(3000);
-        console.log("localhost:", 3000)
+        app.listen(port);
+        console.log("localhost:", port)
     } catch (error) {
         console.error('Unable to concet to the database', error)
     }
